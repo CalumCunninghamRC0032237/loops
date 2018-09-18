f369a328912c4a51b112b3d3ce5154f6d7316db0
RC0032237 <RC0032237@CRO-24-06>
2018-09-18 09:42:00 +0100

    Add project files.

---
+++ loops/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace loops
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int i = 0, < 4, i++)
+            {
+                pictureBox1.Location = new Point(pictureBox1.Location x + 4, pictureBox1, Location.Y);
+                await Task
+            }
+                
+        }
+    }
+}
